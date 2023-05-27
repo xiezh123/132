b2d73f6649c041111ed55fa8c583dbc31178defc
xiezh123 <115209072+xiezh123@users.noreply.github.com>
2023-05-28 02:51:05 +0800

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -73,4 +73,13 @@ else
   echo "已存在 swap 分区的项在 /etc/fstab 中，将不再执行添加命令"
 fi
 
+# 获取 wlan0 网络接口的 IP 地址
+ip_address=$(ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d '/' -f 1)
+
+# 输出提示信息，包含 wlan0 网络接口的 IP 地址，字体为黄色
+echo -e "\033[33m更新过程无法停止，绿色LED会长亮后闪烁，压缩直至重启，建议使用WIFI连接（$ip_address）\033[0m"
+
+# 读取用户的输入
+read -p "请按 Enter 键继续..." choice
+
 shred -u /root/Quick/6.sh
